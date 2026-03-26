f7296ae3b5e20f58d7d8c5930c09741208cb600a
luan-services <luandotservices@gmail.com>
2026-03-25 21:39:42 -0300

    refact: updated astro.config.mjs to use fontProviders to import fonts from google, replaced old assets/fonts folder way

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,28 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig, fontProviders } from 'astro/config';
+import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
 
-import react from '@astrojs/react';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
- site: 'https://www.nandaflores.com.br/',
-  integrations: [sitemap(), react()]
+	vite: {
+		plugins: [tailwindcss()]
+	},
+	site: 'https://www.nandaflores.com.br/',
+	integrations: [sitemap(), react()],
+	fonts: [
+		{
+			provider: fontProviders.google(),
+			name: "Noto Sans",
+			cssVariable: "--font-noto-sans",
+			weights: [400, 500, 600, 700], /* regular medium semibold bold */
+			styles: ["normal"]
+		},
+		{
+			provider: fontProviders.google(),
+			name: "Playfair Display",
+			cssVariable: "--font-playfair-display",
+			weights: [400, 500, 600, 700], 
+			styles: ["normal", "italic"]
+		}
+  	]
 });
